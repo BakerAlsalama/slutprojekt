423a01fa8892025b407a21f6ca8a831bafb791d3
BakerAlsalama <235251614+BakerAlsalama@users.noreply.github.com>
2026-05-19 10:02:14 +0200

    added sidebar and menu button for easier mobile navigation

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("sidenav").style.width = "50%";
+  document.getElementById("overlay").style.display = "inline-block"
+}
+
+function closeNav() {
+  document.getElementById("sidenav").style.width = "0";
+  document.getElementById("overlay").style.display = "none"
+}
